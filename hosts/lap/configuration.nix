296ed9d2567b26fba4e338559527615312a6c399
Jan Kratochvil <jan.kratochvil.94@gmail.com>
2026-05-29 20:06:51 +0200

    configure wireguard to home as a NetwokManager profile, configure dns resolvers

--- hosts/lap/configuration.nix
+++ hosts/lap/configuration.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   lib,
   pkgs,
@@ -121,10 +122,72 @@
     nftables.enable = true;
   };
   services.resolved.enable = true;
+  networking.networkmanager.dns = "systemd-resolved";
 
-  networking.wireguard = {
+  # Temporary local nameserver to resolve kratochvil-jan.eu to private IP
+  # until i fix the DNS on the router
+  networking.nameservers = [ "127.0.0.1" ];
+  services.unbound = {
     enable = true;
-    # TODO
+
+    settings = {
+      server = {
+        interface = [ "127.0.0.1" ];
+        access-control = [ "127.0.0.0/8 allow" ];
+
+        # wildcard zone
+        local-zone = [
+          ''"kratochvil-jan.eu." redirect''
+        ];
+
+        # wildcard answer (this is the key part)
+        local-data = [
+          ''"kratochvil-jan.eu. A 10.0.10.13"''
+        ];
+      };
+    };
+  };
+
+  # Wireguard configuration as a NetworkManager profile
+  # This way i can toggle on/off the connection
+  # via the standard network management applets
+  networking.networkmanager.ensureProfiles.profiles = {
+    # Best way to configure this:
+    # 1. Create a wg.conf and test it via `wg-quick up ./wg.conf`
+    # 2. Once that works, import the conf into NetworkManager,
+    #    via `nmcli connection import type wireguard file ./wg.conf`
+    # 3. If that works, convert this imperative config
+    #    into declarative for NetworkManager:
+    #    `sudo su -c "cd /etc/NetworkManager/system-connections && nix --extra-experimental-features 'nix-command flakes' run github:Janik-Haag/nm2nix | nix --extra-experimental-features 'nix-command flakes' run nixpkgs#nixfmt-rfc-style"`
+    wg = {
+      connection = {
+        id = "wg";
+        interface-name = "wg";
+        type = "wireguard";
+        uuid = "67cb5bbc-e7e3-4aa4-b719-3a43b510e1f8";
+      };
+      ipv4 = {
+        address1 = "192.168.216.4/32";
+        # TODO uncomment this when i fix dns on router via wireguard
+        # dns = "10.0.10.254";
+        # dns-search = "~kratochvil-jan.eu";
+        method = "manual";
+      };
+      ipv6 = {
+        addr-gen-mode = "default";
+        method = "disabled";
+      };
+      proxy = { };
+      wireguard = {
+        listen-port = "51820";
+        private-key = builtins.readFile "${inputs.self.outPath}/secrets/wg/laptop.key";
+      };
+      "wireguard-peer.+uVH6IVVxe1gAGo2JbpGYwU7mOE1FXW+dEMTEqL1AiY=" = {
+        allowed-ips = "10.0.10.0/24;";
+        endpoint = "hjd0a6c2c11.vpn.mynetname.net:23345";
+        persistent-keepalive = "30";
+      };
+    };
   };
 
   # Perihperals
